a34ffa7d6bfc62cf84e110b8eceb3f99e5ab9b63
thiagotmendes <thiagotmendes@outlook.com>
2016-06-07 13:51:15 -0300

    iniciando o projeto brifing 4media

---
+++ resources/views/brifing.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.css" title="bootstrap">
+    <link rel="stylesheet" href="css/estilo.css" title="estilo-global">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <title>Brifing 4media</title>
+
+  </head>
+  <body>
+
+    <header>
+      <div class="container">
+        <div class="text-xs-center">
+          <img src="http://4media.com.br/wp-content/themes/4media2016/images/logo.png" class="center-block" alt="4media">
+        </div>
+      </div>
+      <div class="container">
+        <h2 class="text-center">Bem vindo a 4media Agência Digital</h2>
+        <p class="text-center">
+          Ajude nos a entender suas necessidades e desejos com seu novo projeto preenchendo o maximo de campos abaixo. <br />
+          É muito importante que obetenhamos o máximo de informações possiveis para o melhor desenvolvimento do projeto
+        </p>
+      </div>
+    </header>
+
+
+    <div class="container">
+      <form class="" action="index.html" method="post">
+        <div class="form-group">
+          <label for="">Nome</label>
+          <input type="text" class="form-control" id="" placeholder="">
+          <p class="help-block">Help text here.</p>
+        </div>
+      </form>
+    </div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-1.12.4.min"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.css"></script>
+  </body>
+</html>
